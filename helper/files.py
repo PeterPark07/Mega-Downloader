5ebb5fc6a7cf07d87e10949334eb69d75d1c382f
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-23 17:36:22 +0530

    Update files.py

--- helper/files.py
+++ helper/files.py
@@ -13,3 +13,13 @@ def delete_files_in_directory(directory):
         file_path = os.path.join(directory, filename)
         if os.path.isfile(file_path):
             os.remove(file_path)
+            
+def get_files_in_directory(directory, size_threshold=10):
+    file_paths = []
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            file_size = os.path.getsize(file_path)
+            if file_size >= size_threshold * 1048576:  # Convert size threshold to bytes
+                file_paths.append(file_path)
+    return file_paths[0]
