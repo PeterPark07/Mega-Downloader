7c389b65803485b3ece39a55bd6bfd8a81b12dff
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-23 17:32:38 +0530

    Update mega.py

--- helper/mega.py
+++ helper/mega.py
@@ -25,14 +25,15 @@ def download_file_from_mega(url):
     # Calculate download duration
     end_time = time.time()
 
-    # Get the directory size
-    file_size = get_directory_size(output_path)
+    # Get the directory size in MB
+    file_size = (get_directory_size(output_path))/1048576
 
     duration = end_time - start_time
 
     # Calculate download speed in MB/s
-    download_speed = file_size / duration / 1048576
-
+    download_speed = file_size / duration
+    
     # Print download statistics
     print(f"Download completed in {duration:.2f} seconds.")
+    print(f"File is {file_size} MB in size
     print(f"Download speed: {download_speed:.2f} MBs/second")
