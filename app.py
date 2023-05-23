df0f12117b5d0651e90134fc3b568a2dab3dc433
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-23 18:05:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, send_file
 from helper.mega import download_file_from_mega
 
 
@@ -13,7 +13,7 @@ def download():
     url = request.form['url']
     files = download_file_from_mega(url)
     download_link = files[0]  # Assuming there's only one file
-    return jsonify(download_link=download_link)
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
