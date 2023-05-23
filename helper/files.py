f72089b7693248e1f94496936b610e9b2178fbd9
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-23 17:29:03 +0530

    Create files.py

---
+++ helper/files.py
@@ -0,0 +1,15 @@
+import os
+
+def get_directory_size(directory):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            total_size += os.path.getsize(file_path)
+    return total_size
+
+def delete_files_in_directory(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
